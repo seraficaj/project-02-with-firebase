2a600280dc24f8749a5d7b4e4a5cf26339859a34
teknoformula <seraficaj@gmail.com>
2019-03-05 11:47:14 -0800

    Firebase's Authentication API

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -12,7 +12,22 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+    
+    this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
