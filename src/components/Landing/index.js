0f1b0d1b30bb0fc3a75c99218ddeb3c623e4c57f
lukazdane <deandrevidal@aol.com>
2019-03-06 15:18:12 -0800

    img and slider resized to 50vh, will always take up 50% of the view

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Card } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./Landing.css";
 
 const Landing = () => (
   <div>
@@ -20,18 +22,33 @@ const Landing = () => (
       </div>
     </Carousel>
     {/* This next section is for the cards below the banner */}
-
-    <div class="ui link card">
-      <div class="content">
-        <div class="header">Slaytina</div>
-        <div class="meta">The $7 Billion Woman</div>
-        <div class="description">
-          Legs 5 inches longer than Naomi Smalls With the modelling capabilities
-          of Raja the Gemini A waist 3 inches smaller than Violet Chachki I give
-          you... Slaytina!
-        </div>
-      </div>
-    </div>
+    <Card.Group itemsPerRow={3}>
+      <Card
+        link
+        header="Slaytina"
+        meta="The $7 Billion Woman"
+        description={[
+          "Legs 5 inches longer than Naomi Smalls With the modelling capabilities of Raja the Gemini A waist 3 inches smaller than Violet Chachki I give you... Slaytina!"
+        ].join("")}
+      />
+      <Card
+        link
+        header="Rick Sanchez"
+        meta="Scientist"
+        description={[
+          "Rick is a genius scientist whose alcoholism and reckless,",
+          " nihilistic behavior are a source of concern for his family."
+        ].join("")}
+      />
+      <Card
+        link
+        header="Slaytina"
+        meta="The $7 Billion Woman"
+        description={[
+          "Legs 5 inches longer than Naomi Smalls With the modelling capabilities of Raja the Gemini A waist 3 inches smaller than Violet Chachki I give you... Slaytina!"
+        ].join("")}
+      />
+    </Card.Group>
   </div>
 );
 
