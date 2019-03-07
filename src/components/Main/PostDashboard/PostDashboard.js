e94f27bca9f12fb856ca5a1612ea094853610b34
BryantBuilt <bryantp24@gmail.com>
2019-03-06 16:23:09 -0800

    currentCityId pushes up to main container

--- src/components/Main/PostDashboard/PostDashboard.js
+++ src/components/Main/PostDashboard/PostDashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withAuthorization } from '../../Session';
 import './PostDashboard.css';
+import PostForm from './PostForm/PostForm';
 import PostHeader from './PostHeader/PostHeader';
 import PostList from './PostList/PostList';
 
@@ -9,6 +10,7 @@ class PostDashboard extends Component {
         return (
             <div id='post-dashboard'>
                 <h1>Post Dashboard</h1>
+                <PostForm currentCityId={this.props.currentCityId}/>
                 <PostHeader />
                 <PostList />
             </div>
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import { withAuthorization } from '../Session';
 import './main.css';
+import firebase from 'firebase/app';
 import Cities from './Cities/Cities';
 import PostDashboard from './PostDashboard/PostDashboard';
 
 class Main extends Component {
+  state = {
+    currentCityId: 'san-francisco'
+  }
+
+  handleInput = (e) => {
+    this.setState({ 
+      [e.target.name]: e.target.value 
+    });
+  }
+
+  setCity = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    this.setState({
+      currentCityId: e.target.name
+    })
+  }
+
   render() {
     return (
   <div id='main'>
-    <Cities />
-    <PostDashboard />
+    <h1>Selected City: {this.state.currentCityId}</h1>
+    <Cities 
+      setCity={this.setCity} 
+      currentCityId={this.state.currentCityId}
+    />
+    <PostDashboard 
+      currentCityId={this.state.currentCityId}
+    />
   </div>
     )
   }
