1f042b39ab36a371d53acdadd710d55a8e6a22fd
meltedopen <isaac.chang5@gmail.com>
2019-03-07 15:27:14 -0800

    styled post, postlist, postheader

--- src/components/Main/PostDashboard/PostDashboard.js
+++ src/components/Main/PostDashboard/PostDashboard.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import { withAuthorization } from '../../Session';
-import './PostDashboard.css';
+
 import PostForm from './PostForm/PostForm';
 import PostHeader from './PostHeader/PostHeader';
 import PostList from './PostList/PostList';
 import { withFirebase } from '../../Firebase';
 
 class PostDashboard extends Component {
-    render () {
-        return (
-            <div id='post-dashboard'>
-                <h1>Post Dashboard</h1>
-                <PostForm currentCityId={this.props.currentCityId}/>
-                <PostHeader />
-                <PostList />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="post-dashboard">
+        <h1>Post Dashboard</h1>
+
+        <PostHeader />
+        <PostForm currentCityId={this.props.currentCityId} />
+        <PostList />
+      </div>
+    );
+  }
 }
 
 const condition = authUser => !!authUser;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
 import { withAuthorization } from '../Session';
-import './main.css';
+// import './main.css';
 import Cities from './Cities/Cities';
 import PostDashboard from './PostDashboard/PostDashboard';
 import { withFirebase } from '../Firebase';
+import { Grid } from 'semantic-ui-react';
 
 class Main extends Component {
   state = {
     currentCityId: 'san-francisco'
-  }
+  };
 
-  handleInput = (e) => {
-    this.setState({ 
-      [e.target.name]: e.target.value 
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
     });
-  }
+  };
 
-  setCity = (e) => {
+  setCity = e => {
     e.preventDefault();
     console.log(e.target.name);
     this.setState({
       currentCityId: e.target.name
-    })
-  }
+    });
+  };
 
   render() {
     return (
-  <div id='main'>
-    <h1>Selected City: {this.state.currentCityId}</h1>
-    <Cities 
-      setCity={this.setCity} 
-      currentCityId={this.state.currentCityId}
-    />
-    <PostDashboard 
-      currentCityId={this.state.currentCityId}
-    />
-  </div>
-    )
+      <Grid>
+        <Grid.Column width={6}>
+          <Cities
+            setCity={this.setCity}
+            currentCityId={this.state.currentCityId}
+          />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <PostDashboard currentCityId={this.state.currentCityId} />
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
-
 
 const condition = authUser => !!authUser;
 
