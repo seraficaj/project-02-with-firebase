183c7a8229b6e18d82ccac1c7273979f4bfc38ea
meltedopen <isaac.chang5@gmail.com>
2019-03-08 15:39:03 -0800

    styled account page, added SF pic to carousel, other misc tweaks

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,37 +1,49 @@
 import React from 'react';
 
 import {
+  Container,
   Segment,
   Header,
   Form,
   Divider,
   Label,
   Button,
-  Icon
+  Icon,
+  Grid
 } from 'semantic-ui-react';
 import { AuthUserContext, withAuthorization } from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
+import SettingsNav from './SettingsNav';
+import AccountBasic from './AccountBasic';
 
 const AccountPage = () => (
-  <AuthUserContext.Consumer>
-    {authUser => (
-      <Segment>
-        <Header dividing size="large" content="Account" />
-        <div>
-          <Header color="teal" sub content="Change password" />
-          <p>Use this form to update your account settings</p>
-          {/* <h3>Account: {authUser.email}</h3> */}
-          <Form>
-            {/* <PasswordForgetForm />
+  <Grid>
+    <Grid.Column width={12}>
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <Segment>
+            <Header dividing size="large" content="Account" />
+            <div>
+              <Header color="teal" sub content="Change password" />
+              <p>Use this form to update your account settings</p>
+              {/* <h3>Account: {authUser.email}</h3> */}
+              {/* <Form> */}
+              {/* <PasswordForgetForm />
 
             <Divider /> */}
-            <PasswordChangeForm />
-          </Form>
-        </div>
-      </Segment>
-    )}
-  </AuthUserContext.Consumer>
+
+              <PasswordChangeForm />
+              {/* </Form> */}
+            </div>
+          </Segment>
+        )}
+      </AuthUserContext.Consumer>
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <SettingsNav />
+    </Grid.Column>
+  </Grid>
 );
 
 const condition = authUser => !!authUser;
