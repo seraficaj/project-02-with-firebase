d73ccee5990ebcfe9ff4b804b0483926c9c45a40
lukazdane <deandrevidal@aol.com>
2019-03-10 12:46:03 -0700

    post.js removed disabled

--- src/components/Main/PostDashboard/PostForm/PostForm.js
+++ src/components/Main/PostDashboard/PostForm/PostForm.js
@@ -1,64 +1,62 @@
-import React, { Component } from 'react';
-import { Input, TextArea, Button, Modal, Form } from 'semantic-ui-react';
-import { withAuthorization } from '../../../Session';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import { Input, TextArea, Button, Modal, Form } from "semantic-ui-react";
+import { withAuthorization } from "../../../Session";
+import firebase from "firebase";
 
 class PostForm extends Component {
+  createPost = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    let postdb = firebase.database().ref("post/");
+    let newPostId = postdb.push().key;
+    firebase
+      .database()
+      .ref(`post/${newPostId}`)
+      .update({
+        title: this.state.title,
+        comments: this.state.comments,
+        cityId: this.props.currentCityId
+      });
+  };
 
-    createPost = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        let postdb = firebase.database().ref('post/');
-        let newPostId = postdb.push().key;
-        firebase
-            .database()
-            .ref(`post/${newPostId}`)
-            .update({
-                title: this.state.title,
-                comments: this.state.comments,
-                cityId: this.props.currentCityId
-            });
-    }
-    
-    handleInput = (e) => {
-        this.setState({ 
-            [e.target.name]: e.target.value 
-          }); 
-    }
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    render () {
+  render() {
+    return (
+      <Modal trigger={<Button color="primary">New Post</Button>} closeIcon>
+        <Modal.Header>Submit a New Post</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Field
+              control={Input}
+              label="Post Title"
+              placeholder="Post Title"
+              name="title"
+              onChange={this.handleInput}
+            />
 
-        return (
-          <Modal trigger={<Button color="primary">New Post</Button>} closeIcon>
-            <Modal.Header>Submit a New Post</Modal.Header>
-            <Modal.Content>
-              <Form>
-                <Form.Field
-                  control={Input}
-                  label="Post Title"
-                  placeholder="Post Title"
-                  name='title'
-                  onChange={this.handleInput}
-                />
+            <Form.Field
+              control="textarea"
+              rows="5"
+              control={TextArea}
+              label="Comments"
+              placeholder="Comments"
+              name="comments"
+              onChange={this.handleInput}
+            />
 
-                <Form.Field
-                  control="textarea"
-                  rows="5"
-                  control={TextArea}
-                  label="Comments"
-                  placeholder="Comments"
-                  name='comments'
-                  onChange={this.handleInput}
-                />
-
-                <Button positive type="submit" onClick={this.createPost}>
-                  Submit
-                </Button>
-              </Form>
-            </Modal.Content>
-          </Modal>
-        )
-    }
+            <Button positive type="submit" onClick={this.createPost}>
+              Submit
+            </Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
 }
 
 const condition = authUser => !!authUser;
