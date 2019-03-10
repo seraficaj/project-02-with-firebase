9165583007697f618b5002104bcca3d05ba8b934
meltedopen <isaac.chang5@gmail.com>
2019-03-10 12:40:31 -0700

    updated account settings page styling

--- src/components/Account/AccountBasic.jsx
+++ src/components/Account/AccountBasic.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { Segment, Form, Header, Divider, Button } from 'semantic-ui-react';
+import {
+  Card,
+  Image,
+  Segment,
+  Form,
+  Header,
+  Divider,
+  Button
+} from 'semantic-ui-react';
 
 class AccountBasic extends Component {
   render() {
     return (
       <div>
         <Header color="teal" sub content="Basics" />
+        <p>Use this form to update your account settings</p>
         {/* <Card>
           <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
         </Card> */}
@@ -14,15 +23,21 @@ class AccountBasic extends Component {
             <input type="text" placeholder="Name" />
           </Form.Field>
           {/* <Form.Group inline>TODO: Gender Radio button</Form.Group> */}
+
+          <Form.Field width={8}>
+            <input type="text" placeholder="Current City" />
+          </Form.Field>
           <Form.Field width={8}>
             <label>Join Date:</label>
             <input type="date" placeholder="Join Date" />
           </Form.Field>
-          <Form.Field width={8}>
-            <input type="text" placeholder="Current City" />
-          </Form.Field>
           <Divider />
-          <Button size="large" positive content="Update Profile" />
+          <Button
+            size="large"
+            positive
+            type="submit"
+            content="Update Profile"
+          />
         </Form>
       </div>
     );
--- src/components/Main/PostDashboard/PostForm/PostForm.js
+++ src/components/Main/PostDashboard/PostForm/PostForm.js
@@ -4,60 +4,58 @@ import { withAuthorization } from '../../../Session';
 import firebase from 'firebase';
 
 class PostForm extends Component {
+  createPost = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    let postdb = firebase.database().ref('post/');
+    let newPostId = postdb.push().key;
+    firebase
+      .database()
+      .ref(`post/${newPostId}`)
+      .update({
+        title: this.state.title,
+        comments: this.state.comments,
+        cityId: this.props.currentCityId
+      });
+  };
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    createPost = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        let postdb = firebase.database().ref('post/');
-        let newPostId = postdb.push().key;
-        firebase
-            .database()
-            .ref(`post/${newPostId}`)
-            .update({
-                title: this.state.title,
-                comments: this.state.comments,
-                cityId: this.props.currentCityId
-            });
-    }
-    handleInput = (e) => {
-        this.setState({ 
-            [e.target.name]: e.target.value 
-          });
-    }
+  render() {
+    return (
+      <Modal trigger={<Button color="green">New Post</Button>} closeIcon>
+        <Modal.Header>Submit a New Post</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Field
+              control={Input}
+              label="Post Title"
+              placeholder="Post Title"
+              name="title"
+              onChange={this.handleInput}
+            />
 
-    render () {
+            <Form.Field
+              control="textarea"
+              rows="5"
+              control={TextArea}
+              label="Comments"
+              placeholder="Comments"
+              name="comments"
+              onChange={this.handleInput}
+            />
 
-        return (
-          <Modal trigger={<Button color="primary">New Post</Button>} closeIcon>
-            <Modal.Header>Submit a New Post</Modal.Header>
-            <Modal.Content>
-              <Form>
-                <Form.Field
-                  control={Input}
-                  label="Post Title"
-                  placeholder="Post Title"
-                  name='title'
-                  onChange={this.handleInput}
-                />
-
-                <Form.Field
-                  control="textarea"
-                  rows="5"
-                  control={TextArea}
-                  label="Comments"
-                  placeholder="Comments"
-                  name='comments'
-                  onChange={this.handleInput}
-                />
-
-                <Button positive type="submit" onClick={this.createPost}>
-                  Submit
-                </Button>
-              </Form>
-            </Modal.Content>
-          </Modal>
-        )
-    }
+            <Button positive type="submit" onClick={this.createPost}>
+              Submit
+            </Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
 }
 
 const condition = authUser => !!authUser;
